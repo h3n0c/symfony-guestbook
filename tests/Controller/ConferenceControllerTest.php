7e4c24fa2df47f5e940ece2b29da0001263c3583
HenocKA <henoc.khouilla@gmail.com>
2020-05-30 00:18:56 +0200

    17. Tester

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -15,9 +15,26 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h2', 'Give you feedback!');
     }
 
-    public function testConferencePage()
+
+    public function testCommentSubmission()
     {
         $client = static::createClient();
+        $client->request('GET', '/conference/amsterdam-2019');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Admin',
+            'comment_form[text]'   => 'Some feedback from an automated functional test',
+            'comment_form[email]'  => 'me@automat.ed',
+            'comment_form[photo]'  => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
+        ]);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 2 comments")');
+    }
+
+    public function testConferencePage()
+    {
+        $client  = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertCount(2, $crawler->filter('h4'));
